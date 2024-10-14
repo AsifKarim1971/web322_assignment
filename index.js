b4352523a2b22938f1095b378ee3a021e885f7b6
Md Asif Karim <asif.karim225@gmail.com>
2024-10-14 03:34:19 -0400

    AS2 P5

--- index.js
+++ index.js
@@ -1,52 +1,10 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
-const contentService = require("./content-service");
 
-// Serve static files from the 'public' directory
-app.use(express.static("public"));
-
-// Initialize content service
-contentService.initialize().then(() => {
-    console.log("Content service initialized");
-
-    // Root route that redirects to 'about'
-    app.get("/", (req, res) => {
-        res.redirect("/about");
-    });
-
-    // Serve 'home.html' from the 'views' folder
-    app.get("/home", (req, res) => {
-        res.sendFile(path.join(__dirname, "views", "home.html"));
-    });
-
-    // Serve 'about.html' from the 'views' folder
-    app.get("/about", (req, res) => {
-        res.sendFile(path.join(__dirname, "views", "about.html"));
-    });
-
-    // Route for serving articles
-    app.get("/articles", (req, res) => {
-        contentService.getPublishedArticles().then(articles => {
-            res.json(articles);
-        }).catch(err => {
-            res.status(500).json({ message: "Error retrieving articles", error: err });
-        });
-    });
-
-    // Route for serving categories
-    app.get("/categories", (req, res) => {
-        contentService.getCategories().then(categories => {
-            res.json(categories);
-        }).catch(err => {
-            res.status(500).json({ message: "Error retrieving categories", error: err });
-        });
-    });
-
-}).catch(err => {
-    console.error("Failed to initialize content service:", err);
+app.get('/', (req, res) => {
+    res.send('If you see this, the deployment is working!');
 });
 
-// Export the app for Vercel deployment, no listening here
 module.exports = app;
+
 
