efe5781fd11cf5d6558976dffa89bc706eb5ce66
Md Asif Karim <asif.karim225@gmail.com>
2024-10-14 04:05:41 -0400

    AS2 P5

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+
+app.get("/articles", (req, res) => {
+    contentService.getPublishedArticles().then(articles => {
+        res.json(articles);
+    }).catch(err => {
+        console.error("Failed to retrieve articles:", err);
+        res.status(500).json({ message: "Error retrieving articles", error: err });
+    });
 });
 
 module.exports = app;
