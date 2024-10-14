ad86544c575b5ea02029dac970c8f9fa27a7dc44
Md Asif Karim <asif.karim225@gmail.com>
2024-10-14 03:47:33 -0400

    AS2 P5

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ app.use(express.static('public'));
 const path = require('path');
 
 app.get('/home', (req, res) => {
-    res.type('html');
+    console.log('Serving:', path.join(__dirname, 'views', 'home.html'));
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
+app.get('/about', (req, res) => {
+    console.log('Serving:', path.join(__dirname, 'views', 'about.html'));
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
 
 module.exports = app;
 
