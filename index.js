8826c9d1cb1818e20945dfeb9a2344e13c625fa0
Md Asif Karim <asif.karim225@gmail.com>
2024-10-14 04:16:45 -0400

    AS2 P5

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ const app = express();
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Root route redirects to home
 app.get('/', (req, res) => {
-    res.redirect('/home');
+    res.redirect('/home');  // Ensure this redirection is correct
 });
 
-// Home route
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));  // Check this path
 });
 
-// About route
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));  // And this path
 });
 
 module.exports = app;
