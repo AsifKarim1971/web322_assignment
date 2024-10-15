e0e17a67d3aca4d3f753beb01714fb62058480f5
Asif Karim <asif.karim225@gmail.com>
2024-10-14 23:27:33 -0400

    Update content-service.js

--- content-service.js
+++ content-service.js
@@ -1,38 +1,41 @@
 const fs = require("fs");
+const path = require("path");
 
 let articles = [];
 let categories = [];
 
+const articlesPath = path.resolve(__dirname, './data/articles.json');
+const categoriesPath = path.resolve(__dirname, './data/categories.json');
+
 module.exports = {
   initialize: function () {
     return new Promise((resolve, reject) => {
-      fs.readFile("./data/articles.json", "utf8", (err, data) => {
+      fs.readFile(articlesPath, "utf8", (err, data) => {
         if (err) {
-          reject("unable to read file");
-        } else {
-          articles = JSON.parse(data);
-
-          fs.readFile("./data/categories.json", "utf8", (err, data) => {
-            if (err) {
-              reject("unable to read file");
-            } else {
-              categories = JSON.parse(data);
-              resolve();
-            }
-          });
+          console.error("Error reading articles file:", err);
+          reject(err);
+          return;
         }
+        articles = JSON.parse(data);
+        fs.readFile(categoriesPath, "utf8", (err, data) => {
+          if (err) {
+            console.error("Error reading categories file:", err);
+            reject(err);
+            return;
+          }
+          categories = JSON.parse(data);
+          resolve();
+        });
       });
     });
   },
   getPublishedArticles: function () {
     return new Promise((resolve, reject) => {
-      const publishedArticles = articles.filter(
-        (article) => article.published === true
-      );
+      const publishedArticles = articles.filter(article => article.published === true);
       if (publishedArticles.length > 0) {
         resolve(publishedArticles);
       } else {
-        reject("no results returned");
+        reject(new Error("No published articles found."));
       }
     });
   },
@@ -41,7 +44,7 @@ module.exports = {
       if (categories.length > 0) {
         resolve(categories);
       } else {
-        reject("no results returned");
+        reject(new Error("No categories found."));
       }
     });
   },
